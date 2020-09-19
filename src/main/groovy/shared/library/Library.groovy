9c3da63240ef645a2fa8b0f31ef9a4b0a47622b0
arvid.erickson@gmail.com <jenkins-x@googlegroups.com>
2020-09-19 14:16:40 -0700

    Pipeline library test 1

--- src/main/groovy/shared/library/Library.groovy
+++ src/main/groovy/shared/library/Library.groovy
@@ -7,4 +7,15 @@ class Library {
     boolean someLibraryMethod() {
         true
     }
+    def execute() {
+        stage("Stage 1") {
+            echo "Executing Stage 1"
+          }
+        stage("Stage 2") {
+            echo "Executing Stage 2"
+          }
+        stage("Stage 3") {
+            echo "Executing Stage 3"
+          }
+    }
 }
